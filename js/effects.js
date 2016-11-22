d15d6d1b7f236c053f126cff5e2591a057432cf0
Jordan <jhealth7@gmail.com>
2016-11-21 20:43:47 -0800

    updates

--- js/effects.js
+++ js/effects.js
@@ -1,71 +1,86 @@
 //==============================================//
-                //★ EFFECTS ★//
+//★ EFFECTS ★//
 //==============================================//
 (function() {
-  var support = { transitions: Modernizr.csstransitions },
-    // transition end event name
-    transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    onEndTransition = function( el, callback ) {
-      var onEndCallbackFn = function( ev ) {
-        if( support.transitions ) {
-          if( ev.target != this ) return;
-          this.removeEventListener( transEndEventName, onEndCallbackFn );
+    var support = {
+            transitions: Modernizr.csstransitions
+        },
+        // transition end event name
+        transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+        onEndTransition = function(el, callback) {
+            var onEndCallbackFn = function(ev) {
+                if (support.transitions) {
+                    if (ev.target != this) return;
+                    this.removeEventListener(transEndEventName, onEndCallbackFn);
+                }
+                if (callback && typeof callback === 'function') {
+                    callback.call(this);
+                }
+            };
+            if (support.transitions) {
+                el.addEventListener(transEndEventName, onEndCallbackFn);
+            } else {
+                onEndCallbackFn();
+            }
+        };
+
+    new GridFx(document.querySelector('.grid'), {
+        imgPosition: {
+            x: 1,
+            y: -0.75
+        },
+        onOpenItem: function(instance, item) {
+            var win = {
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+            instance.items.forEach(function(el) {
+                if (item != el) {
+                    var delay = Math.floor(Math.random() * 130);
+                    el.style.WebkitTransition = 'opacity .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1), -webkit-transform .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1)';
+                    el.style.transition = 'opacity .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1), transform .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1)';
+
+                    var elOffset = el.getBoundingClientRect(),
+                        elSize = {
+                            width: el.offsetWidth,
+                            height: el.offsetHeight
+                        };
+
+                    el.style.WebkitTransform = 'translate3d(' + (win.width / 2 - elOffset.left - elSize.width / 2) + 'px,' + (win.height - elOffset.top - elSize.height / 2) + 'px,0) scale3d(0,0,1)';
+                    el.style.transform = 'translate3d(' + (win.width / 2 - elOffset.left - elSize.width / 2) + 'px,' + (win.height - elOffset.top - elSize.height / 2) + 'px,0) scale3d(0,0,1)';
+                    el.style.opacity = 0;
+                }
+            });
+        },
+        onCloseItem: function(instance, item) {
+            instance.items.forEach(function(el) {
+                if (item != el) {
+                    el.style.WebkitTransition = 'opacity .3s, -webkit-transform .3s';
+                    el.style.transition = 'opacity .3s, transform .3s';
+
+                    el.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
+                    el.style.transform = 'translate3d(0,0,0) scale3d(1,1,1)';
+                    el.style.opacity = 1;
+
+                    onEndTransition(el, function() {
+                        el.style.transition = 'none';
+                        el.style.WebkitTransform = 'none';
+                    });
+                }
+            });
         }
-        if( callback && typeof callback === 'function' ) { callback.call(this); }
-      };
-      if( support.transitions ) {
-        el.addEventListener( transEndEventName, onEndCallbackFn );
-      }
-      else {
-        onEndCallbackFn();
-      }
-    };
-
-  new GridFx(document.querySelector('.grid'), {
-    imgPosition : {
-      x : 1,
-      y : -0.75
-    },
-    onOpenItem : function(instance, item) {
-      var win = {width: window.innerWidth, height: window.innerHeight};
-      instance.items.forEach(function(el) {
-        if(item != el) {
-          var delay = Math.floor(Math.random() * 130);
-          el.style.WebkitTransition = 'opacity .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1), -webkit-transform .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1)';
-          el.style.transition = 'opacity .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1), transform .5s ' + delay + 'ms cubic-bezier(.7,0,.3,1)';
-
-          var elOffset = el.getBoundingClientRect(),
-            elSize = {width : el.offsetWidth, height : el.offsetHeight};
-
-          el.style.WebkitTransform = 'translate3d(' + (win.width/2 - elOffset.left - elSize.width/2) + 'px,' + (win.height - elOffset.top - elSize.height/2) + 'px,0) scale3d(0,0,1)';
-          el.style.transform = 'translate3d(' + (win.width/2 - elOffset.left - elSize.width/2) + 'px,' + (win.height - elOffset.top - elSize.height/2) + 'px,0) scale3d(0,0,1)';
-          el.style.opacity = 0;
-        }
-      });
-    },
-    onCloseItem : function(instance, item) {
-      instance.items.forEach(function(el) {
-        if(item != el) {
-          el.style.WebkitTransition = 'opacity .3s, -webkit-transform .3s';
-          el.style.transition = 'opacity .3s, transform .3s';
-
-          el.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
-          el.style.transform = 'translate3d(0,0,0) scale3d(1,1,1)';
-          el.style.opacity = 1;
-
-          onEndTransition(el, function() {
-            el.style.transition = 'none';
-            el.style.WebkitTransform = 'none';
-          });
-        }
-      });
-    }
-  });
+    });
 })();
 
 //==============================================//
-                //★ STARS ★//
+//★ STARS ★//
 //==============================================//
 var starCount = 70;
 var delayTimeMax = 7;
@@ -87,17 +102,31 @@ for (var i = 0; i < starCount; i++) {
 
     var starLight = $("<li>", {
         class: "starlight"
-            }).css({
-            top: starTop, //height random
-            left: starLeft, //width random
-            "-webkit-animation-delay": delaytime + 's', //randomize by 7(delaytimemax)
-            });
-            starContainer.append(starLight); //apppending starLight to .star_container
-            }
+    }).css({
+        top: starTop, //height random
+        left: starLeft, //width random
+        "-webkit-animation-delay": delaytime + 's', //randomize by 7(delaytimemax)
+    });
+    starContainer.append(starLight); //apppending starLight to .star_container
+}
+var googleA = (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-87796241-1', 'auto');
+ga('send', 'pageview');;
 //==============================================//
-                //★ DYANMIC TEXT ★//
+//★ DYANMIC TEXT ★//
 //==============================================//
 title.innerHTML = 'Jordan Kimura';
+document.getElementsByTagName('head').innerHTML = googleA;
 profileImg.innerHTML = '<img class="img_profile" src="img/original/digital/jordan.png">';
 greeting1.innerHTML = "Hi! I'm Jordan.";
 greeting2.innerHTML = "I'm a UI/UX Strategist, Front-End Developer, Illustrator and Green Drink Enthusiast.";
@@ -111,7 +140,7 @@ aboutHeader.innerHTML = 'About';
 aboutP1.innerHTML = 'Well hello there, thanks for stopping by!';
 aboutP2.innerHTML = 'My name is Jordan Kimura and I earned a B.A. in Digital Design and an M.B.A. with an emphasis in Strategic Marketing while playing college tennis. Passionate about creating meaningful things that are enriching, I strive to implement well-branded creative assets and content deliverables.';
 aboutDevSkills.innerHTML = 'Development Skills';
-aboutDevIcons.innerHTML = '<img src="img/icons/06_icons.png">'+'<img src="img/icons/07_icons.png">' + '<img src="img/icons/08_icons.png">' + '<img src="img/icons/09_icons.png">' + '<img src="img/icons/10_icons.png">' + '<img src="img/icons/12_icons.png">' +'<img src="img/icons/11_icons.png">';
+aboutDevIcons.innerHTML = '<img src="img/icons/06_icons.png">' + '<img src="img/icons/07_icons.png">' + '<img src="img/icons/08_icons.png">' + '<img src="img/icons/09_icons.png">' + '<img src="img/icons/10_icons.png">' + '<img src="img/icons/12_icons.png">' + '<img src="img/icons/11_icons.png">';
 aboutDesignSkills.innerHTML = 'Design Skills';
 aboutDesignIcons.innerHTML = '<img src="img/icons/01_icons.png"> ' + '<img src="img/icons/02_icons.png"> ' + '<img src="img/icons/03_icons.png"> ' + '	<img src="img/icons/04_icons.png"> ' + '<img src="img/icons/05_icons.png">';
 
