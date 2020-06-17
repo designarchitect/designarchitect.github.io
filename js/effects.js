f3beebec326d37c6dc277f42ece0226cf65ff5b8
JK <gingerbread@jks-macbook-pro.attlocal.net>
2020-06-17 12:41:28 -0700

    minor updates

--- js/effects.js
+++ js/effects.js
@@ -128,7 +128,7 @@ for (var i = 0; i < starCount; i++) {
 //★ DYANMIC TEXT ★//
 //==============================================//
 title.innerHTML = 'Jordan Kimura';
-profileImg.innerHTML = '<img class="img_profile" src="img/original/digital/jordan.png">';
+profileImg.innerHTML = '<img class="img_profile" alt="Jordan" width="50" src="img/original/digital/jordan.png">';
 greeting1.innerHTML = "Hi! I'm Jordan.";
 greeting2.innerHTML = "I'm a UI/UX Designer, Developer,";
 greeting3.innerHTML = "and Wellness Enthusiast.";
@@ -140,18 +140,19 @@ portfolioSection3.innerHTML = 'Print';
 portfolioSection4.innerHTML = 'Photography';
 portfolioSection5.innerHTML = 'Illustration';
 aboutHeader.innerHTML = 'About';
-aboutP1.innerHTML = 'Well hello there, thanks for stopping by!';
+aboutP1.innerHTML = 'Hello there, thanks for stopping by!';
 aboutP2.innerHTML = 'My name is Jordan Kimura and I earned a B.A. in Graphic Design and an M.B.A. with an emphasis in Strategic Marketing while playing college tennis. Passionate about creating meaningful things that are enriching, I strive to implement well-branded creative assets and content deliverables.';
 aboutDevSkills.innerHTML = 'Development Skills';
 aboutDevIcons.innerHTML = '<img src="img/icons/06_icons.png" alt="HTML"  data-toggle="tooltip" data-placement="bottom" title="HTML">' + '<img src="img/icons/07_icons.png" alt="CSS" data-toggle="tooltip" data-placement="bottom" title="CSS">' + '<img src="img/icons/08_icons.png" alt="JavaScript" data-toggle="tooltip" data-placement="bottom" title="JavaScript">' + '<img src="img/icons/14_icons.png" alt="Angular" data-toggle="tooltip" data-placement="bottom" title="Angular">' + '<img src="img/icons/09_icons.png" alt="jQuery" data-toggle="tooltip" data-placement="bottom" title="jQuery">' + '<img src="img/icons/10_icons.png" alt="SASS" data-toggle="tooltip" data-placement="bottom" title="SASS">' + '<img src="img/icons/12_icons.png" alt="Bootstrap" data-toggle="tooltip" data-placement="bottom" title="Bootstrap">' + '<img src="img/icons/11_icons.png" alt="Github" data-toggle="tooltip" data-placement="bottom" title="Github">';
 aboutDesignSkills.innerHTML = 'Design Skills';
-aboutDesignIcons.innerHTML = '<img src="img/icons/13_icons.png" alt="Sketch" data-toggle="tooltip" data-placement="bottom" title="Sketch"> ' + '<img src="img/icons/01_icons.png" alt="Photoshop" data-toggle="tooltip" data-placement="bottom" title="Photoshop"> ' + '<img src="img/icons/02_icons.png" alt="Illustrator" data-toggle="tooltip" data-placement="bottom" title="Illustrator"> ' + '<img src="img/icons/03_icons.png" alt="After Effects" data-toggle="tooltip" data-placement="bottom" title="After Effects"> ' + '	<img src="img/icons/04_icons.png" alt="Premiere" data-toggle="tooltip" data-placement="bottom" title="Premiere"> ' + '<img src="img/icons/05_icons.png" alt="InDesign" data-toggle="tooltip" data-placement="bottom" title="InDesign">';
+aboutDesignIcons.innerHTML = '<img src="img/icons/13_icons.png" alt="Sketch" data-toggle="tooltip" data-placement="bottom" title="Sketch"> ' + '<img src="img/icons/15_icons.jpg" alt="InVision" data-toggle="tooltip" data-placement="bottom" title="InVision"> ' + '<img src="img/icons/01_icons.png" alt="Photoshop" data-toggle="tooltip" data-placement="bottom" title="Photoshop"> ' + '<img src="img/icons/02_icons.png" alt="Illustrator" data-toggle="tooltip" data-placement="bottom" title="Illustrator"> ' + '<img src="img/icons/03_icons.png" alt="After Effects" data-toggle="tooltip" data-placement="bottom" title="After Effects"> ' + '	<img src="img/icons/04_icons.png" alt="Premiere" data-toggle="tooltip" data-placement="bottom" title="Premiere"> ' + '<img src="img/icons/05_icons.png" alt="InDesign" data-toggle="tooltip" data-placement="bottom" title="InDesign">';
 
 //resumeBtn.innerHTML = '<a class="a_resume" href="" target="_blank">View Resume </a><i class="fa fa-file-pdf-o" aria-hidden="true"></i>';
 email.innerHTML = '<i class="fa fa-envelope" aria-hidden="true"></i>' + '<a href="mailto:jordantennis7@gmail.com" class="email"> jordantennis7@gmail.com</a>';
 location1.innerHTML = 'South Orange County, CA';
 location2.innerHTML = 'United States';
-contactIcons.innerHTML = '<a class="contactIcon" href="https://www.linkedin.com/in/jordankimura" target="_blank" alt="Jordan Kimura"><i class="fa fa-linkedin fa-2x animated" aria-hidden="true"></i></a>' + '<a class="contactIcon" href="https://github.com/designarchitect" target="_blank" alt="designarchitect"><i class="fa fa-github fa-2x animated" aria-hidden="true"></i></a>';
+contactIcons.innerHTML = '<a class="contactIcon" href="https://www.linkedin.com/in/jordankimura" target="_blank" alt="Jordan Kimura"><i class="fa fa-linkedin fa-2x animated" aria-hidden="true"></i></a>';
+//contactIcons.innerHTML = '<a class="contactIcon" href="https://www.linkedin.com/in/jordankimura" target="_blank" alt="Jordan Kimura"><i class="fa fa-linkedin fa-2x animated" aria-hidden="true"></i></a>' + '<a class="contactIcon" href="https://github.com/designarchitect" target="_blank" alt="designarchitect"><i class="fa fa-github fa-2x animated" aria-hidden="true"></i></a>';
 var currentYearPara = document.getElementById('currentYear');
 currentYearPara.innerHTML = 'Jordan Kimura &copy; 2011 - ' + new Date().getFullYear();
 poweredBy.innerHTML = 'Powered by <a href="https://github.com" target="_blank" alt="github">Github</a>.';
